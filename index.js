506a0a156e3acc4f546debe9f9bf861dc802414b
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-01 16:23:33 -0800

    made all questions employess intern engineer

--- index.js
+++ index.js
@@ -42,3 +42,141 @@ const managerQuestions = [
         name: 'office', 
     },
 ];
+
+function init() {
+    inquirer.prompt (managerQuestions).then((answers) => {
+      const manager = new Manager(
+        answers.firstName + " " + answers.lastName,
+        answers.id,
+        answers.email,
+        answers.office
+      );
+      employees.push(manager);
+      return askPromptQuestions();
+    });
+}
+
+const mainQuestions = [
+    {
+        type: "list",
+        message: "Which kind of employee would you like to add",
+        choices: ["Intern", "Engineer", "Exit"],
+        name: "employeeType",
+    },
+];
+
+function askPromptQuestions() {
+    inquirer
+      .prompt(mainQuestions)
+  
+      // data = giant object (answers from input)
+      .then((answers) => {
+        // console.log(answers);
+        if (answers.employeeType === "Intern") {
+          promptInternQuestions();
+        }
+        if (answers.employeeType === "Engineer") {
+          promptEngineerQuestions();
+        }
+        if (answers.employeeType === "Exit") {
+          exit();
+          // end program when select exit
+        }
+      });
+}
+
+ // function to prompt intern questions
+ function promptInternQuestions() {
+    // 4 questions: intern’s name, ID, email, and school
+    const internQuestions = [
+        {
+            type: "input",
+            message: "What is your intern's first name?",
+            name: "firstName",
+          },
+      
+          {
+            type: "input",
+            message: "What is your intern's last name?",
+            name: "lastName",
+          },
+      
+          {
+            type: "input",
+            message: "What is your intern's ID?",
+            name: "id",
+          },
+      
+          {
+            type: "input",
+            message: "What is your intern's email address?",
+            name: "email",
+          },
+      
+          {
+            type: "input",
+            message: "What is your intern's school?",
+            name: "school",
+          },
+    ];
+    inquirer.prompt(internQuestions).then((answers) => {
+      const intern = new Intern(
+        answers.firstName + " " + answers.lastName,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      
+      employees.push(intern);
+      return askPromptQuestions();
+    });
+}
+
+function promptEngineerQuestions() {
+    // name, ID, email, and GitHub username
+    const engineerQuestions = [
+      {
+        type: "input",
+        message: "What is your engineer's first name?",
+        name: "firstName",
+      },
+  
+      {
+        type: "input",
+        message: "What is your engineer's last name?",
+        name: "lastName",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's email address?",
+        name: "email",
+      },
+  
+      {
+        type: "input",
+        message: "What is your engineer's Github username?",
+        name: "github",
+      },
+    ];
+    inquirer.prompt(engineerQuestions).then((answers) => {
+      const engineer = new Engineer(
+        answers.firstName + " " + answers.lastName,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      employees.push(engineer);
+      return askPromptQuestions();
+    });
+}
+
+function exit() {
+    writeToFile("index.html", employees);
+}
+
+init();
